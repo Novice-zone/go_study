1aac06c037041b62ca33a824ffbe02382a8f1b30
dfdstick <z18523157968@163.com>
2025-03-26 00:59:42 +0800

    go语言运算符

---
+++ basicPra3.go
@@ -0,0 +1,50 @@
+package main
+
+// GO语言运算符
+func main() {
+	/*a := 10
+	b := 20
+	//fmt.Printf("%d\n%d", a, b)
+
+	//算术运算
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+
+	//++和--不是运算符而是单独的语句，不存在前后区别
+	//a=b++，就是错误的
+	a++
+	b++
+	fmt.Printf("%d\n%d\n", a, b)
+
+	//关系运算，与C语言相同
+	fmt.Println(a < b)
+	fmt.Println(a >= b)
+	fmt.Println(a == b)*/
+
+	/*//逻辑运算 && || !
+
+	fmt.Println(12 > 15 && 1 != 0) //false  &&有false就是false
+	fmt.Println(!(5 != 8))         //false
+	fmt.Println(1 < 2 || 1 > 2)    //true ||有true就是true
+	//位运算& | ^
+	n := 1 //001
+	m := 3 //011
+	fmt.Println(n, m)
+	n = n & m //001  有0就是0
+	fmt.Println(n)
+	n = n | m //011  //有1就是1
+	fmt.Println(n)
+	n = 1
+	n = n ^ m //010 异或  对应位不一样则为1
+	fmt.Println(n)
+	n = 1
+	n = n << 3 //左移n位就是乘以2的n次方 001->1000
+	fmt.Println(n)
+	n = n >> 3 //右移n位就是除以2的n次方 1000->0001
+	fmt.Println(n)*/
+
+	//赋值运算符 先运算了再赋值
+	//= += -= *= /= %= <<= >>= &= |= ^=
+}
